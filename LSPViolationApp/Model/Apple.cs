f82b2e1accd5f6807310df5a43b9e5b2ca229969
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:24:05 +0530

    Add files via upload

---
+++ LSPViolationApp/Model/Apple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSPViolationApp.Model
+{
+    class Apple
+    {
+        public virtual string GetColor()
+        {
+            return "Red";
+        }
+    }
+}
---
+++ LSPViolationApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LSPViolationApp.Model;
+
+namespace LSPViolationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apple ap = new Orange();
+            Console.WriteLine("Color of Apple is: "+ap.GetColor());
+            Console.ReadLine();
+        }
+    }
+}
